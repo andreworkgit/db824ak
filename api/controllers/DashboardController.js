82245bf4dc364352d659efa9373f7ea3db923031
Andre <andrework@gmail.com>
2013-11-16 23:58:50 -0200

    ts

---
+++ api/controllers/DashboardController.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  'index': function (req, res,next) {
+  	/*var oldDateObj = new Date();
+  	var newDateObj = new Date(oldDateObj.getTime() + 60000);
+  	req.session.cookie.expires = newDateObj;
+  	req.session.authenticated = true;
+
+  	console.log(req.session);
+
+  	res.locals.flash = _.clone(req.session.flash);
+    res.view('session/new');
+    req.session.flash = {};*/
+    res.view('dashboard/index');
+  },
+
+}
