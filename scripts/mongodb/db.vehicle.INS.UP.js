48f041bbcdac55b6eef25d599a619ae2a3bded7a
Andre <andrework@gmail.com>
2013-11-20 12:59:32 -0200

    ts

---
+++ scripts/mongodb/db.vehicle.INS.UP.js
@@ -0,0 +1,23 @@
+db.vehicle.update({},{
+	ref: '1',
+	name: 'A pé',
+	zeroto100km: '5'
+},{
+	upset:true
+});
+
+db.vehicle.update({},{
+	ref: '2',
+	name: 'Fuscão xp',
+	zeroto100km: '30'
+},{
+	upset:true
+});
+
+db.vehicle.update({},{
+	ref: '2',
+	name: 'Celto NPM',
+	zeroto100km: '28'
+},{
+	upset:true
+});
---
+++ scripts/mongodb/place_insert.js
@@ -0,0 +1,27 @@
+
+db.place.insert({
+	ref: '1',
+	name: 'Padaria',
+	picture:'padaria',
+	distance: '200'
+});
+
+
+db.place.insert({
+	ref: '2',
+	name: 'Banca de Jornal',	
+	picture:'banca-de-jornal',
+	distance: '240'
+});
+
+db.place.insert({
+	ref: '3',
+	name: 'Doceria',
+	picture:'doceria',
+	distance: '320'
+});
+
+
+
+
+
