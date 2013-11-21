57b8768fac6c8d7db3219565fdb5e5bfc992f855
Andre Augusto <andrework@gmail.com>
2013-11-21 15:04:10 -0200

    ts

---
+++ scripts/mongodb/db.place.INS.UP.js
@@ -0,0 +1,28 @@
+
+db.place.update({},{
+	ref: '1',
+	name: 'Padaria',
+	picture:'padaria',
+	distance: '200'
+},{
+	upset:true
+});
+
+
+db.place.update({},{
+	ref: '2',
+	name: 'Banca de Jornal',	
+	picture:'banca-de-jornal',
+	distance: '240'
+},{
+	upset:true
+});
+
+db.place.update({},{
+	ref: '3',
+	name: 'Doceria',
+	picture:'doceria',
+	distance: '320'
+},{
+	upset:true
+});
