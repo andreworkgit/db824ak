bc029769feca0bade0ff1ab04e681eb5fdc84389
Andre Augusto <andrework@gmail.com>
2013-11-22 17:08:53 -0200

    ts

--- README.md
+++ README.md
@@ -18,10 +18,20 @@ start server
 mongod --dbpath C:/mongodb/data/db
 
 write script in mongo
-mongo 127.0.0.1/sails-dobanks C:/nodejs/node_modules/sails/bin/projetos/dobanks/scripts/mongodb/locais.js
+mongo 127.0.0.1/sails-dobanks C:/nodejs/node_modules/sails/bin/projetos/dobanks/scripts/mongodb
 mongo 127.0.0.1/sails-dobanks C:/nodejs/projetos/dobanks/scripts/mongodb/place_updateAndInsert.js
 
 db.place.find().forEach(printjson)
+
+UPDATE COMUM PARA TODOS
+db.user.update({provider:"google"},{ $inc: {transportation: 1}},{multi:true});
+db.user.update(<where>,{ $inc: <set>},{multi:<all>});
+
+UPDATE SE NAO TIVER CRIA (http://docs.mongodb.org/manual/reference/operator/update/push/#up._S_push)
+db.user.update({provider:"google"},{ $push: {vehicles: { $each:  [1,2]} } },{multi:true,upsert:true});
+
+REMOVE CAMPOS DE UM COLLECTION 
+db.user.update({provider:"google"},{ $unset : {vehicles: ""}},{multi:true});
 
 wget
 wget -c -r -P C:/fileswget http://www.
--- scripts/mongodb/db.vehicle.INS.UP.js
+++ scripts/mongodb/db.vehicle.INS.UP.js
@@ -35,51 +35,82 @@ db.vehicle.insert({
 	mkmperhour: '14'
 });*/
 
-db.vehicle.update({},{
-	ref: '1',
-	name: 'A pé',
-	mkmperhour: '5'
-},{
-	upset:true
-});
+db.vehicle.findAndModify( {
+    query: {ref: '1'},
+    update: { 
+    	$set: {	
+    			ref: '1',
+				name: 'A pé',
+				mkmperhour: '5'
+			}
+    },
+    upsert: true,
+    new: true
+} );
 
-db.vehicle.update({},{
-	ref: '2',
-	name: 'Bicicleta BMX',
-	mkmperhour: '8'
-},{
-	upset:true
-});
+db.vehicle.findAndModify( {
+    query: {ref: '2'},
+    update: { 
+    	$set: {	
+				ref: '2',
+				name: 'Bicicleta BMX',
+				mkmperhour: '8'
+			}
+    },
+    upsert: true,
+    new: true
+} );
 
-db.vehicle.update({},{
+db.vehicle.findAndModify( {
+    query: {ref: '3',},
+    update: { 
+    	$set: {	
 	ref: '3',
 	name: 'Mountain Bike',
 	mkmperhour: '11'
-},{
-	upset:true
-});
+			}
+    },
+    upsert: true,
+    new: true
+} );
 
-
-db.vehicle.update({},{
+db.vehicle.findAndModify( {
+    query: {ref: '4'},
+    update: { 
+    	$set: {	
 	ref: '4',
 	name: 'Ferdin GT10',
 	mkmperhour: '14'
-},{
-	upset:true
-});
+			}
+    },
+    upsert: true,
+    new: true
+} );
 
-db.vehicle.update({},{
+db.vehicle.findAndModify( {
+    query: {ref: '5'},
+    update: { 
+    	$set: {	
 	ref: '5',
 	name: 'Bugan GS3',
 	mkmperhour: '14'
-},{
-	upset:true
-});
+			}
+    },
+    upsert: true,
+    new: true
+} );
 
-db.vehicle.update({},{
+db.vehicle.findAndModify( {
+    query: {ref: '6'},
+    update: { 
+    	$set: {	
 	ref: '6',
 	name: 'Risco LP5',
 	mkmperhour: '14'
-},{
-	upset:true
-});
+			}
+    },
+    upsert: true,
+    new: true
+} );
+
+
