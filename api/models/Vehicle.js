da67385a18e229e3e1f9f9c9021bdb044f66d1fa
Andre <andrework@gmail.com>
2013-11-21 22:59:19 -0200

    ts

---
+++ api/models/Vehicle.js
@@ -0,0 +1,20 @@
+
+
+module.exports = {
+
+  schema:true,
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+
+	ref: 'integer',
+	name: 'string',  
+	mkmperhour: 'integer'
+    
+  },
+
+
+
+};
